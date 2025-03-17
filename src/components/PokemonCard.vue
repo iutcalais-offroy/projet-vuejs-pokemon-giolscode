21c11c47f96b51f63528cb505d91ae93f4d474ad
LOSAT Giovanni <giolstpro@gmail.com>
2025-03-17 13:39:56 +0100

    Liste v1.0

---
+++ src/components/PokemonCard.vue
@@ -0,0 +1,52 @@
+<template>
+  <n-card :title="pokemon.name">
+
+    <template #cover>
+      <img :src="pokemon.imageUrl" alt="pokemon.name" class="pokemon-image" />
+    </template>
+
+    <div class="pokemon-details">
+      <p> <strong> Type: </strong> {{ pokemon.type.name }}</p>
+      <p> <strong> HP: </strong> {{ pokemon.lifePoints }} | <strong> Taille: </strong> {{ pokemon.lifePoints }}  | <strong> Poids : </strong> {{ pokemon.weight }} </p>
+      <p> <strong> Attaque: </strong> {{ pokemon.attack.name }} | <strong> Dégât : </strong> {{ pokemon.attack.damages }} </p>
+    </div>
+
+  </n-card>
+
+</template>
+
+<script lang="ts" setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  pokemon: {
+    type: Object,
+    required: true,
+  },
+});
+</script>
+
+<style scoped>
+.n-card {
+  max-width: 300px;
+  margin: 1em;
+  margin-left: auto;
+  margin-right: auto;
+  border-radius: 8px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
+
+}
+.pokemon-image {
+  width: 100%;
+  height: auto;
+  border-radius: 8px 8px 0 0;
+}
+.pokemon-details {
+  margin-top: 1em;
+  padding: 1em;
+  background-color: #f9f9f9;
+  border-radius: 0 0 8px 8px;
+  box-shadow: 0 -2px 8px rgba(0, 0, 0, 0.1);
+  
+}
+</style>
--- src/pages/DeckBuilder.page.vue
+++ src/pages/DeckBuilder.page.vue
@@ -1,11 +1,55 @@
 <template>
-  Complete this template
+  <div>
+    <h1>Deck Builder</h1>
+    <div class="pokemon-list">
+      <PokemonCard v-for="pokemon in pokemons" :key="pokemon.id" :pokemon="pokemon" />
+    </div>
+  </div>
 </template>
 
 <script lang="ts" setup>
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+import PokemonCard from '../components/PokemonCard.vue';
 
+interface Pokemon {
+  id: number;
+  name: string;
+  imageUrl: string;
+  type: string;
+  hp: number;
+  attack: number;
+}
+
+const pokemons = ref<Pokemon[]>([]);
+
+const fetchPokemons = async () => {
+  try {
+    const response = await axios.get('https://pokemon-api-seyrinian-production.up.railway.app/pokemon-cards');
+    pokemons.value = response.data;
+  } catch (error) {
+    console.error('Erreur lors de la récupération des Pokémon:', error);
+  }
+};
+
+onMounted(fetchPokemons);
 </script>
 
 <style scoped>
-
+.pokemon-list {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1em;
+}
+.pokemon-card {
+  max-width: 300px;
+}
+.pokemon-card img {
+  width: 100%;
+  height: auto;
+}
+h1 {
+  text-align: center;
+  margin-bottom: 2em;
+}
 </style>
